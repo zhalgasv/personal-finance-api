4e1aabde05ddfb9234d5c233516ac68210b950f8
zhalgasadil <zhalgasvactor@gmail.com>
2026-06-01 18:07:54 +0500

    Initial commit

---
+++ src/main/java/com/zhalgas/personalfinanceapi/PersonalFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.zhalgas.personalfinanceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceApiApplication.class, args);
+	}
+
+}
