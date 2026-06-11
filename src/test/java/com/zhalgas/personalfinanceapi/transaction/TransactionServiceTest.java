948303ec7a153870b72c956f86958a9eac540de7
zhalgasadil <zhalgasvactor@gmail.com>
2026-06-11 16:20:10 +0500

    Add unit tests for create and update transaction methods in TransactionService; handle user existence checks and transaction retrieval

--- src/test/java/com/zhalgas/personalfinanceapi/transaction/TransactionServiceTest.java
+++ src/test/java/com/zhalgas/personalfinanceapi/transaction/TransactionServiceTest.java
@@ -10,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ import static org.mockito.Mockito.anyLong;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
@@ -156,5 +158,166 @@ class TransactionServiceTest {
        verify(userRepository).findByUsername("adil");
        verify(transactionRepository).findByIdAndUserId(1L, 1L);
        verify(transactionRepository, never()).delete(any(Transaction.class));
+    }
+
+    @Test
+    void createTransaction_shouldCreateTransactionWhenUserExists() {
+        User user = User.builder()
+                .id(1L)
+                .username("adil")
+                .build();
+
+        TransactionDto dto = new TransactionDto();
+        dto.setDescription("Groceries");
+        dto.setType(TransactionType.EXPENSE);
+        dto.setAmount(new BigDecimal("150"));
+        dto.setDate(LocalDate.now());
+
+        when(userRepository.findByUsername("adil"))
+                .thenReturn(Optional.of(user));
+        when(transactionRepository.save(any(Transaction.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        TransactionDto result = transactionService.createTransaction("adil", dto);
+
+        assertEquals(new BigDecimal("150"), result.getAmount());
+        assertEquals("Groceries", result.getDescription());
+        assertEquals(TransactionType.EXPENSE, result.getType());
+
+        verify(userRepository).findByUsername("adil");
+        verify(transactionRepository).save(any(Transaction.class));
+    }
+
+    @Test
+    void createTransaction_shouldThrowExceptionWhenUserNotFound() {
+
+        TransactionDto dto = new TransactionDto();
+        dto.setDescription("Groceries");
+        dto.setType(TransactionType.EXPENSE);
+        dto.setAmount(new BigDecimal("150"));
+        dto.setDate(LocalDate.now());
+
+        when(userRepository.findByUsername("adil"))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () ->
+                transactionService.createTransaction("adil", dto));
+
+        verify(transactionRepository, never()).save(any(Transaction.class));
+    }
+
+    @Test
+    void updateTransaction_shouldUpdateTransactionWhenExists() {
+
+        User user = User.builder()
+                .id(1L)
+                .username("adil")
+                .build();
+
+        TransactionDto dto = new TransactionDto();
+        dto.setDescription("Groceries");
+        dto.setType(TransactionType.EXPENSE);
+        dto.setAmount(new BigDecimal("150"));
+        dto.setDate(LocalDate.now());
+
+        Transaction transaction = Transaction.builder()
+                .id(1L)
+                .amount(new BigDecimal("100"))
+                .description("Old Description")
+                .type(TransactionType.EXPENSE)
+                .date(LocalDate.now().minusDays(1))
+                .build();
+
+        when(userRepository.findByUsername("adil"))
+                .thenReturn(Optional.of(user));
+
+        when(transactionRepository.findByIdAndUserId(1L, 1L))
+                .thenReturn(Optional.of(transaction));
+
+        when(transactionRepository.save(any(Transaction.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        TransactionDto result = transactionService.updateTransaction("adil", 1L, dto);
+
+        assertEquals(new BigDecimal("150"), result.getAmount());
+        assertEquals("Groceries", result.getDescription());
+        assertEquals(TransactionType.EXPENSE, result.getType());
+        assertEquals(LocalDate.now(), result.getDate());
+
+        verify(userRepository).findByUsername("adil");
+        verify(transactionRepository).save(any(Transaction.class));
+        verify(transactionRepository).findByIdAndUserId(1L, 1L);
+    }
+
+    @Test
+    void updateTransaction_shouldThrowExceptionWhenTransactionNotFound() {
+        User user = User.builder()
+                .id(1L)
+                .username("adil")
+                .build();
+
+        TransactionDto dto = new TransactionDto();
+        dto.setDescription("Groceries");
+        dto.setType(TransactionType.EXPENSE);
+        dto.setAmount(new BigDecimal("150"));
+        dto.setDate(LocalDate.now());
+
+        when(userRepository.findByUsername("adil"))
+                .thenReturn(Optional.of(user));
+        when(transactionRepository.findByIdAndUserId(1L, 1L))
+                .thenReturn(Optional.empty());
+
+        assertThrows(TransactionNotFoundException.class, () ->
+                transactionService.updateTransaction("adil", 1L, dto));
+
+        verify(userRepository).findByUsername("adil");
+        verify(transactionRepository).findByIdAndUserId(1L, 1L);
+        verify(transactionRepository, never()).save(any(Transaction.class));
+    }
+
+    @Test
+    void getTransactions_shouldReturnUserTransactions() {
+        User user = User.builder()
+                .id(1L)
+                .username("adil")
+                .build();
+
+        Transaction transaction1 = Transaction.builder()
+                .id(1L)
+                .amount(new BigDecimal("5000"))
+                .description("Salary")
+                .type(TransactionType.INCOME)
+                .date(LocalDate.now().minusDays(5))
+                .user(user)
+                .build();
+
+        Transaction transaction2 = Transaction.builder()
+                .id(2L)
+                .amount(new BigDecimal("150"))
+                .description("Groceries")
+                .type(TransactionType.EXPENSE)
+                .date(LocalDate.now().minusDays(2))
+                .user(user)
+                .build();
+
+        when(userRepository.findByUsername("adil"))
+                .thenReturn(Optional.of(user));
+
+        when(transactionRepository.findByUserIdOrderByDateDesc(1L))
+                .thenReturn(List.of(transaction1, transaction2));
+
+        List<TransactionDto> result = transactionService.getTransactionsByUsername("adil", null, null, null);
+
+        assertEquals(2, result.size());
+        assertEquals(new BigDecimal("5000"), result.get(0).getAmount());
+        assertEquals("Salary", result.get(0).getDescription());
+        assertEquals(TransactionType.INCOME, result.get(0).getType());
+
+        assertEquals(new BigDecimal("150"), result.get(1).getAmount());
+        assertEquals("Groceries", result.get(1).getDescription());
+        assertEquals(TransactionType.EXPENSE, result.get(1).getType());
+
+        verify(userRepository).findByUsername("adil");
+        verify(transactionRepository).findByUserIdOrderByDateDesc(1L);
     }
 }
